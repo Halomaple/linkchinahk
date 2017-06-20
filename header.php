3424d67bd7d95a3d841032901fefeef15e78998d
Jianfeng <jeff.doyle@foxmail.com>
2017-06-20 16:56:07 +0800

    get rid of border in post and set .container's width to 100% in small screen

--- header.php
+++ header.php
@@ -66,7 +66,7 @@
 
 		<!--Customized css-->
 		<link href="<?php bloginfo('template_directory'); ?>/css/bootsnav.css" rel="stylesheet"/>
-		<link href="<?php bloginfo('template_directory'); ?>/style.css?v=1.0.2" rel="stylesheet"/>
+		<link href="<?php bloginfo('template_directory'); ?>/style.css?v=1.0.4" rel="stylesheet"/>
 	</head>
 
 	<body>
