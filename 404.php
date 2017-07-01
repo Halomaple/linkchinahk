c3f0cc56f3cd021f9e8b60eb54f4facf38ec194c
Jianfeng <jeff.doyle@foxmail.com>
2017-07-01 20:10:08 +0800

    404 style

--- 404.php
+++ 404.php
@@ -3,7 +3,7 @@
 <div class="container">
 	<div class="row">
 		<div class="col-xs-12 col-md-12 col-lg-12">
-			<div class="not-found-h1">
+			<div class="page-not-found">
 				<img src="<?php bloginfo('template_directory')?>/images/404.jpg"/>
 			</div>
 		</div><!--/col-xs-12 col-md-9 -->
