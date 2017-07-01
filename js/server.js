00f4f8c7b4b98338e42570e92994886744942028
Jianfeng <jeff.doyle@foxmail.com>
2017-07-01 17:59:33 +0800

    basic price calculation for room hk and bandwith

--- js/server.js
+++ js/server.js
@@ -1,6 +1,16 @@
 $(document).ready(function() {
+	var configurationsList = {
+		collocationRooms: [],
+		collocationSizes: [],
+		deviceTypes: [],
+		bandwidths: 1
+	};
+
 	var selectedConfigurations = {
-		collocationRoom: '',
+		collocationRoom: {
+			id: 0,
+			name: ''
+		},
 		collocationSize: '',
 		deviceType: '',
 		bandwidth: 1
@@ -16,15 +26,34 @@ $(document).ready(function() {
 	}
 
 	function initializeCollocationRoom() {
+		$('.configuration-collocation-room a').each(function(index) {
+			configurationsList.collocationRooms.push({
+				id: index,
+				name: $(this).text()
+			});
+		});
+
 		$('.configuration-collocation-room a').click(function() {
-			$(this).siblings().removeClass('btn-primary');
-			$(this).addClass('btn-primary');
-			selectedConfigurations.collocationRoom = $(this).text();
+			var element = this;
+			$(element).siblings().removeClass('btn-primary');
+			$(element).addClass('btn-primary');
+
+			selectedConfigurations.collocationRoom.id = configurationsList.collocationRooms.filter(function(r) {
+				return r.name == $(element).text();
+			})[0].id;
+
 			calculateTotalPrice();
 		});
 	}
 
 	function initializeCollocationSize() {
+		$('.configuration-collocation-size a').each(function(index) {
+			configurationsList.collocationSizes.push({
+				id: index,
+				name: $(this).text()
+			});
+		});
+
 		$('.configuration-collocation-size a').click(function() {
 			$(this).siblings().removeClass('btn-primary');
 			$(this).addClass('btn-primary');
@@ -34,6 +63,12 @@ $(document).ready(function() {
 	}
 
 	function initializeDeviceType() {
+		$('.configuration-device-type a').each(function(index) {
+			configurationsList.deviceTypes.push({
+				id: index,
+				name: $(this).text()
+			});
+		});
 		$('.configuration-device-type a').click(function() {
 			$(this).siblings().removeClass('btn-primary');
 			$(this).addClass('btn-primary');
@@ -45,7 +80,7 @@ $(document).ready(function() {
 	function initialBandwith() {
 		var bandwidthSlider = $('#bandwith-slider').slider({
 			tooltip: 'always',
-			ticks: [0, 100, 200, 300, 400, 500, 1000],
+			ticks: [1, 100, 200, 300, 400, 500, 1000],
 			ticks_positions: [0, 10, 20, 30, 50, 70, 100],
 			ticks_labels: ['1M', '100M', '200M', '300M', '400M', '500M', '1000M'],
 			ticks_snap_bounds: 2,
@@ -57,7 +92,7 @@ $(document).ready(function() {
 		$('#bandwith-slider').on('slide', function(slideEvt) {
 			$('#bandwith-value').val(slideEvt.value);
 			syncBandwidth(bandwidthSlider, slideEvt.value);
-		}).on('click mousedown',function(){
+		}).on('click mousedown', function() {
 			$('#bandwith-value').val(bandwidthSlider.slider('getValue'));
 		});
 
@@ -80,12 +115,18 @@ $(document).ready(function() {
 
 	function syncBandwidth(bandwidthSlider, value) {
 		bandwidthSlider.slider('setValue', value);
-		selectedConfigurations.bandwidth = value;
+		selectedConfigurations.bandwidth = parseInt(value);
 		calculateTotalPrice();
 	}
 
 	function calculateTotalPrice() {
-		var price = 100;
+		var price = 0;
+
+		switch (selectedConfigurations.collocationRoom.id) {
+			case configurationsList.collocationRooms[0].id:{
+				price = selectedConfigurations.bandwidth * 450 + 650 ;
+			}
+		}
 
 		$('.total-price-box input').val(price);
 	}
