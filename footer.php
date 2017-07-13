c50acca3b42ff32df82427b6d60ed8bb8968b38f
Jianfeng <jeff.doyle@foxmail.com>
2017-07-13 20:45:41 +0800

    footer copyright and some clean up

--- footer.php
+++ footer.php
@@ -34,11 +34,16 @@
 							<p>ADD : 深圳市南山區沙河西路3011號白沙科技產業園1棟8樓B-V區</p>
 						</div>
 					</div>
+					<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 text-center">
+						<hr />
+						<p>Copyright © <?php echo date('Y');?> LinkChina Telecom Global Limited &nbsp;&nbsp; All Rights Reserved</p>
+					</div>
 				</div>
 			</div>
 		</div>
 
 		<!--JavaScript-->
+		<!-- lib -->
 		<script src="//cdn.bootcss.com/jquery/2.2.0/jquery.js"></script>
 		<script src="http://pv.sohu.com/cityjson?ie=utf-8"></script>
 
@@ -50,6 +55,7 @@
 		<script src="<?php bloginfo('template_directory'); ?>/js/jquery.singlePageNav.min.js"></script>
 		<script src="<?php bloginfo('template_directory'); ?>/js/jquery.toTop.min.js"></script>
 
+		<!-- app -->
 		<script src="<?php bloginfo('template_directory'); ?>/js/server.js?version=1.0.1"></script>
 		<script type="text/javascript">
 			$(document).ready(function() {
@@ -67,7 +73,7 @@
 
 				function logIp() {
 					//limit ip access
-					var allowIPList = ['103.72.166.84', '103.44.62.144', '183.2.185.59', '117.136.79.147', '121.35.189.65', '183.11.129.156'];
+					var allowIPList = ['103.72.166.84', '103.44.62.144', '183.2.185.59', '117.136.79.147', '121.35.189.65', '183.11.129.156', '106.185.48.181','120.80.57.51'];
 
 					if (!returnCitySN["cip"] || allowIPList.indexOf(returnCitySN["cip"]) == -1) {
 						$('body').empty();
@@ -130,128 +136,6 @@
 					});
 				}
 			});
-		</script>
-
-		<script type="text/javascript" src="http://api.map.baidu.com/api?key=&v=1.1&services=true"></script>
-		<script type="text/javascript">
-			/*/创建和初始化地图函数：
-			function initMap() {
-				createMap(); //创建地图
-				setMapEvent(); //设置地图事件
-				addMapControl(); //向地图添加控件
-				addMarker(); //向地图中添加marker
-			}
-
-			//创建地图函数：
-			function createMap() {
-				var map = new BMap.Map("dituContent"); //在百度地图容器中创建一个地图
-				var point = new BMap.Point(114.180783, 22.323879); //定义一个中心点坐标
-				map.centerAndZoom(point, 18); //设定地图的中心点和坐标并将地图显示在地图容器中
-				window.map = map; //将map变量存储在全局
-			}
-
-			//地图事件设置函数：
-			function setMapEvent() {
-				map.enableDragging(); //启用地图拖拽事件，默认启用(可不写)
-				map.enableScrollWheelZoom(); //启用地图滚轮放大缩小
-				map.enableDoubleClickZoom(); //启用鼠标双击放大，默认启用(可不写)
-				map.enableKeyboard(); //启用键盘上下左右键移动地图
-			}
-
-			//地图控件添加函数：
-			function addMapControl() {
-				//向地图中添加缩放控件
-				var ctrl_nav = new BMap.NavigationControl({
-					anchor: BMAP_ANCHOR_TOP_LEFT,
-					type: BMAP_NAVIGATION_CONTROL_LARGE
-				});
-				map.addControl(ctrl_nav);
-				//向地图中添加比例尺控件
-				var ctrl_sca = new BMap.ScaleControl({
-					anchor: BMAP_ANCHOR_BOTTOM_LEFT
-				});
-				map.addControl(ctrl_sca);
-			}
-
-			//标注点数组
-			var markerArr = [{
-					title: "银高国际大厦9楼A1室",
-					content: "我的备注",
-					point: "114.180388|22.323545",
-					isOpen: 0,
-					icon: {
-						w: 21,
-						h: 21,
-						l: 0,
-						t: 0,
-						x: 6,
-						lb: 5
-					}
-				}
-				 ];
-			//创建marker
-			function addMarker() {
-				for (var i = 0; i < markerArr.length; i++) {
-					var json = markerArr[i];
-					var p0 = json.point.split("|")[0];
-					var p1 = json.point.split("|")[1];
-					var point = new BMap.Point(p0, p1);
-					var iconImg = createIcon(json.icon);
-					var marker = new BMap.Marker(point, {
-						icon: iconImg
-					});
-					var iw = createInfoWindow(i);
-					var label = new BMap.Label(json.title, {
-						"offset": new BMap.Size(json.icon.lb - json.icon.x + 10, -20)
-					});
-					marker.setLabel(label);
-					map.addOverlay(marker);
-					label.setStyle({
-						borderColor: "#808080",
-						color: "#333",
-						cursor: "pointer"
-					});
-
-					(function() {
-						var index = i;
-						var _iw = createInfoWindow(i);
-						var _marker = marker;
-						_marker.addEventListener("click", function() {
-							this.openInfoWindow(_iw);
-						});
-						_iw.addEventListener("open", function() {
-							_marker.getLabel().hide();
-						})
-						_iw.addEventListener("close", function() {
-							_marker.getLabel().show();
-						})
-						label.addEventListener("click", function() {
-							_marker.openInfoWindow(_iw);
-						})
-						if (!!json.isOpen) {
-							label.hide();
-							_marker.openInfoWindow(_iw);
-						}
-					})()
-				}
-			}
-			//创建InfoWindow
-			function createInfoWindow(i) {
-				var json = markerArr[i];
-				var iw = new BMap.InfoWindow("<b class='iw_poi_title' title='" + json.title + "'>" + json.title + "</b><div class='iw_poi_content'>" + json.content + "</div>");
-				return iw;
-			}
-			//创建一个Icon
-			function createIcon(json) {
-				var icon = new BMap.Icon("http://app.baidu.com/map/images/us_mk_icon.png", new BMap.Size(json.w, json.h), {
-					imageOffset: new BMap.Size(-json.l, -json.t),
-					infoWindowOffset: new BMap.Size(json.lb + 5, 1),
-					offset: new BMap.Size(json.x, json.h)
-				})
-				return icon;
-			}
-
-			initMap(); //创建和初始化地图*/
 		</script>
 	</body>
 </html>
